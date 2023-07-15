933211d69dde2f8930a29df34af4a671acb65a2d
YusifQasim <yusifqasim22@gmail.com>
2023-07-15 06:07:47 +0300

    update email link

--- KurdDevs/src/main/java/com/KurdDevs/KurdDevs/KurdDevsApplication.java
+++ KurdDevs/src/main/java/com/KurdDevs/KurdDevs/KurdDevsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KurdDevsApplication {
 
-	public String PORT=System.getenv("PORT");
+//	public String PORT=System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(KurdDevsApplication.class, args);
